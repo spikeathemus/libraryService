ebeaa42042d5947aefc0fd5b9686d43370726591
michal turocha <michalturocha@gmail.com>
2020-02-18 11:16:45 +0100

    New branch created, getters and setters for Book entity

--- src/main/java/com/libraryservice/demo/entity/BookEntity.java
+++ src/main/java/com/libraryservice/demo/entity/BookEntity.java
@@ -12,4 +12,28 @@ public class BookEntity {
 
     public BookEntity() {
     }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
 }
